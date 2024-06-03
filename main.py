56773647238c781bfa033fe678b604c70557b5c1
Lex <104356670+imMRroot@users.noreply.github.com>
2024-06-03 04:01:17 +0300

    Update main.py
    global update

--- main.py
+++ main.py
@@ -3,58 +3,69 @@
 import random
 import time
 
-import playwright
-from bs4 import BeautifulSoup
-from loguru import logger
-from playwright.sync_api import sync_playwright
-
-
-def create_soup_obj(html_doc):
-    """Создает объект bs4"""
-    soup = BeautifulSoup(html_doc, 'lxml')
-    return soup
+from playwright.sync_api import sync_playwright, Page
 
 
 class YaParserMap:
     # base_url: str = 'https://yandex.ru/maps/14/tver/chain/pyatyorochka/6003206/?ll=35.890582%2C56.855086&sll=35.890582%2C56.854725&sspn=0.242214%2C0.045331&z=12'
     base_url: str = 'https://yandex.ru/maps/14/tver/?ll=35.911851%2C56.859561&z=12'
     url_tver = 'https://yandex.ru/maps/14/tver/?ll=35.911851%2C56.859561&z=12'
+    wait_text: str = "Добавьте организацию или объект, если не нашли их на карте."
+    data: list = []
+
+    def creates_a_sheet_with_dicts(self, page: Page):
+        """Соберет данные из ul.search-list-view__list """
+        search_elem = page.query_selector('ul')
+        print(search_elem.text_content())
+        elements = search_elem.query_selector_all('li.search-snippet-view')
+
+        for element in elements:
+            self.data.append(
+                {
+                    'coor': element.get_attribute('data-coordinates'),
+                    'title': element.query_selector('.search-business-snippet-view__title').text_content(),
+                    'working': element.query_selector('.business-working-status-view').text_content(),
+                    'address': element.query_selector('.search-business-snippet-view__address').text_content(),
+
+                }
+
+            )
+        print(self.data)
+        return self.data
 
     @classmethod
     def __gen_timeout(cls, item: bool):
         """Генерирует случайное число в диапазоне max от 000 до 30000, min 2000 до 5000. return float"""
         if item:
-            return float(random.randrange(2000, 5000))
+            return float(random.randrange(2000, 3000))
 
         return float(random.randrange(5000, 30000))
 
     @classmethod
-    def __sidebar_scroll_duration(cls, page: playwright):
-        """Вернет True если 1 значение '490px' прекратило увеличиваться в translate3d(0px, 490px, 0px)"""
-        checks_value = page.locator('.scroll__scrollbar-thumb')
-
-    @classmethod
-    def __arrow_down(cls, page: playwright):
+    def __arrow_down(cls, page: Page):
         """Скролит сайдбар в низ до последнего элемента"""
         timeout = cls.__gen_timeout(True)
-        for _ in range(30):  # TODO: Подумать о автоматическом кол-во итераций рассмотреть page.wait_for_selector
-            page.locator("body").press("ArrowDown")
-            page.locator("body").press("ArrowDown")
-            page.locator("body").press("ArrowDown")
-            page.locator("body").press("ArrowDown")
-            page.wait_for_timeout(timeout)
-            page.locator("body").press("ArrowDown")
-            page.locator("body").press("ArrowDown")
-            page.locator("body").press("ArrowDown")
-            page.locator("body").press("ArrowDown")
-            page.locator("body").press("ArrowDown")
 
-    @logger.catch
+        page.locator(".sidebar-view__panel").first.click()
+        txt = page.locator('.scroll__content')
+
+        while True:
+            print('запускаем цикл строка 59 \n')
+
+
+            if txt.get_by_text(cls.wait_text, exact=False)  == 'undefined':
+
+                page.locator("body").press("ArrowDown", timeout=100.0)
+            else:
+                print('условие')
+
+                break
+
     def page_maps_pars(self):
         """Взять данные со страницы"""
         timeout = self.__gen_timeout(True)
 
-        with sync_playwright() as pw:
+        with (sync_playwright() as pw):
             browser = pw.chromium.launch(
                 headless=False,
                 slow_mo=100,
@@ -64,41 +75,29 @@ class YaParserMap:
 
             )
 
-            context = browser.new_context()
+            context = browser.new_context(
+                locale='ru-RU',
+                timezone_id='Europe/Moscow',
+                geolocation={"longitude": 35.911851, "latitude": 56.859561},
+                permissions=["geolocation"]
+            )
             # playwright_spy.load_sync(context) не использовать, часто кидает на clout
+
             page = context.new_page()
 
             page.goto(self.base_url)
             page.wait_for_timeout(timeout)
-            page.get_by_placeholder("Поиск мест и адресов").click()
-            page.wait_for_timeout(3300.0)
-            page.get_by_placeholder("Поиск мест и адресов").fill("пятерочка", timeout=timeout)
-            page.get_by_placeholder("Поиск мест и адресов").press("Enter")
-
+            page.get_by_placeholder('Поиск мест и адресов').type(
+                text='пятёрочка',
+                delay=200.9)
             page.wait_for_timeout(timeout)
-
-            page.locator(".sidebar-view__panel").first.click()
-
+            page.get_by_placeholder('Поиск мест и адресов').press(
+                "Enter",
+                delay=100.0)
             page.wait_for_timeout(timeout)
 
             self.__arrow_down(page)
-            elem = page.query_selector(".sidebar-view__panel")
-
-            tag_li = elem.query_selector('.search-list-view__list')
-            page.pause()
-
-
-            with open('page_ya.html', 'a', encoding='UTF-8') as f:
-                f.writelines(tag_li.input_value())
-
-        # waite_elem = page.wait_for_selector(f':text("Добавьте организацию или объект, если не нашли их на карт")')
-
-        # page.locator("body").press("ArrowDown")
-        # page.get_by_text("Добавьте организацию или объект, если не нашли их на карте").click()
-        #
-
-        # with open('page_ya.html', 'a', encoding='UTF-8') as f:
-        #     f.write(page.content())
+            self.creates_a_sheet_with_dicts(page)
 
         print('ok')
         time.sleep(4)
@@ -112,4 +111,3 @@ def main():
 if __name__ == '__main__':
     main()
     time.sleep(10)
-
